008cded70c5a4545e5654c48f72ac201c56df3a6
zlhliu <1194428282@qq.com>
2023-02-17 15:43:53 +0800

    ok

--- lib/decoder.hpp
+++ lib/decoder.hpp
@@ -1,11 +1,6 @@
 #ifndef _DECODER_H_
 #define _DECODER_H_
 #include<bits/stdc++.h>
-<<<<<<< HEAD
-
-=======
-#include<unordered_set>
->>>>>>> 936f85f (累了，先这样吧)
 class decoder
 {
 private:
@@ -15,12 +10,10 @@ private:
     std::vector<std::vector<double>> _likelyhood_value; // likelyhood value of every layer
     std::unordered_set<int> _frozen_channel;
     void code_calculate();
-<<<<<<< HEAD
-    void sc_calculate(const std::unordered_set<int>& frozen_channe);
-=======
+
     void sc_calculate(const std::unordered_set<int>& frozen_channel);
     // void fsc_calculate(const std::unordered_set<int>& frozen_channel);
->>>>>>> 936f85f (累了，先这样吧)
+
 public:
     std::vector<int> result(const std::vector<int>& Q);
     decoder(const std::vector<double>& likely_value,const std::unordered_set<int>& frozen_channel,std::string decoder_type);
